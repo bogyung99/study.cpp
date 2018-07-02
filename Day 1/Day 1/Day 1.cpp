ee788c736d611cf2dfd8d130372e1d8b0ec4d014
bogung99 <bogung1208@naver.com>
2018-07-02 16:19:58 +0900

    first init

---
+++ Day 1/Day 1/Day 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+// c++ 헤더파일
+// return 은 써도 되고 안써도 되고
+
+using namespace std;
+// c++ 출력을 사용하기 위함
+
+// 기본적인 출력
+int main()
+{
+	cout << "안녕 C++" << "나는" << "나야" << "반가워" << endl;
+
+	return 0;
+}
